b35b759e20af471f4b78defa293d6c668bd1c99a
masalamind <themasalacollective@gmail.com>
2022-05-09 05:35:35 +0300

    Add wtf-forms to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from .config import DevConfig
 
 app = Flask(__name__)
 
+app.config.from_object(DevConfig)
 from app import views
 
 @app.route('/')
 @app.route('/pitches')
-def index('/'):
-    return render_template('index.html')
+def index():
+    return("<h2>This Works</h2>")
+    # return render_template('index.html')
